a344e1d84a180fe0c46ecebe6b2ec87c4a9016dd
Nyaladzi Trust Tjibha <tjibhanyaladzi@gmail.com>
2025-11-20 10:15:23 +0200

    Refactor project structure and add analysis controller
    Removed folder reference for "Controllers" in the project file, indicating a potential restructuring. Added `NumberWordAnalyzerController` to handle HTTP POST requests for analyzing number words, including model validation and error handling.

---
+++ NumberWordAnalyzer/Controllers/NumberWordAnalyzerController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NumberWordAnalyzer.Application;
+using NumberWordAnalyzer.Services;
+
+namespace NumberWordAnalyzer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NumberWordAnalyzerController : ControllerBase
+    {
+        private readonly IAnalyzerService _analyzerService;
+
+        public NumberWordAnalyzerController(IAnalyzerService analyzerService)
+        {
+            _analyzerService = analyzerService;
+        }
+
+        [HttpPost]
+        public IActionResult Analyze([FromBody] AnalyzeNumberWordDto input)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                var cleanInputText = (input.InputText ?? string.Empty).Trim();
+
+                var analysisResult = _analyzerService.AnalyzeText(cleanInputText);
+                return Ok(analysisResult);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    error = "An unexpected error occured.",
+                    details = ex.Message
+                });
+            }
+        }
+    }
+}
